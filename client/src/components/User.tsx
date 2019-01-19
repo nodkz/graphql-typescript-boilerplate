d815c938d5e98ed124dd7f1bd02f7ac46dc3d1cf
nodkz <pavel.chertorogov@gmail.com>
2019-01-20 03:12:55 +0600

    tune lint-staged and husky for pre commit hooks

--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -17,7 +17,7 @@ class User extends Component {
 export const UserQuery = gql`
   query User($id: Int!) {
     user(id: $id) {
-      name123456
+      name
       articles {
         ...aaa
       }
