40ad4ca72aee6db010c2390a82934c815092f828
nodkz <pavel.chertorogov@gmail.com>
2019-01-22 00:11:50 +0600

    add DataLoader for Employee.reportsTo

--- server/src/schema/Employee/employee.resolvers.ts
+++ server/src/schema/Employee/employee.resolvers.ts
@@ -1,5 +1,6 @@
 import { IResolvers } from '../__generated__/graphql';
 import { chain } from 'lodash';
+import DataLoader from 'dataloader';
 import _employeesData from '../__data__/employees.json';
 
 const employeesData = chain(
@@ -8,16 +9,27 @@ const employeesData = chain(
   })
 );
 
+// HACK for logging
+function withLog(data: any) {
+  const ids = Array.isArray(data) ? data.map(o => o._id) : [data._id];
+  console.log(
+    `[${new Date().getSeconds()}] Call DB: returned ${ids.length} records with ids [${ids.join(
+      ','
+    )}]`
+  );
+  return data;
+}
+
 export { employeesData };
 
 const resolvers: IResolvers = {
   Query: {
     employee: (_, args) => {
-      return (employeesData.find({ _id: args.id }).value() as any) || null;
+      return withLog(employeesData.find({ _id: args.id }).value() as any) || null;
     },
     employees: (_, args) => {
       return (
-        (employeesData
+        withLog(employeesData
           .filter({ ...args.filter })
           .drop(args.offset || 0)
           .take(args.limit)
@@ -27,14 +39,41 @@ const resolvers: IResolvers = {
   },
   Employee: {
     reportsID: ({ reportsTo }: any) => reportsTo,
-    reportsTo: ({ reportsTo }: any) => {
+    // reportsTo: ({ reportsTo }: any) => {
+    //   if (!reportsTo || !Number.isInteger(reportsTo)) {
+    //     return null;
+    //   }
+    //   return withLog(employeesData.find({ employeeID: reportsTo }).value() as any);
+    // },
+    reportsTo: ({ reportsTo }: any, _, context, info) => {
+      const { dataloaders } = context;
+
+      // единожы инициализируем DataLoader для получения авторов по ids
+      let dl = dataloaders.get(info.fieldNodes);
+      if (!dl) {
+        dl = new DataLoader(async (ids: any[]) => {
+          // обращаемся в базу чтоб получить авторов по ids
+          const rows = await withLog(
+            employeesData
+              .keyBy('_id')
+              .at(ids)
+              .value()
+          );
+          // IMPORTANT: сортируем данные из базы в том порядке, как нам передали ids
+          const sortedInIdsOrder = ids.map(id => rows.find((x: any) => x._id === id));
+          return sortedInIdsOrder;
+        });
+        // ложим инстанс дата-лоадера в WeakMap для повторного использования
+        dataloaders.set(info.fieldNodes, dl);
+      }
+
       if (!reportsTo || !Number.isInteger(reportsTo)) {
         return null;
       }
-      return employeesData.find({ employeeID: reportsTo }).value() as any;
+      return dl.load(reportsTo) as any;
     },
     subordinates: ({ employeeID }) => {
-      return employeesData.filter({ reportsTo: employeeID }).value() as any;
+      return withLog(employeesData.filter({ reportsTo: employeeID }).value() as any);
     },
   },
 };
--- server/src/schema/context.ts
+++ server/src/schema/context.ts
@@ -1,11 +1,15 @@
 import { Request } from 'express';
+import { FieldNode } from 'graphql';
+import DataLoader from 'dataloader';
 
 export interface GraphQLContext {
   ip: string;
+  dataloaders: WeakMap<ReadonlyArray<FieldNode>, DataLoader<string, object>>;
 }
 
 export async function prepareContext({ req }: { req: Request }): Promise<GraphQLContext> {
   return {
     ip: req.ip,
+    dataloaders: new WeakMap(),
   };
 }
